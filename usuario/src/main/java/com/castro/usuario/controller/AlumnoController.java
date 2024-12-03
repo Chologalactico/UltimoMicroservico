5b396c568c2e900190eeae6b1bc84a6e56bde5b1
Chologalactico <116129679+Chologalactico@users.noreply.github.com>
2024-12-02 23:46:12 -0500

    [ADD]UltimArchivo

---
+++ usuario/src/main/java/com/castro/usuario/controller/AlumnoController.java
@@ -0,0 +1,35 @@
+package com.castro.usuario.controller;
+
+import controller.CommonController;
+import models.entity.Alumno;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.castro.usuario.service.AlumnoService;
+
+import java.util.Optional;
+
+@RestController
+public class AlumnoController extends CommonController<Alumno, AlumnoService> {
+
+    @Value("${config.balanceador.test}")
+    private String balanceadorTest;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id) {
+        Optional <Alumno> ob = service.findById(id);
+        if (ob.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        Alumno alumnoDb = ob.get();
+        alumnoDb.setNombre(alumno.getNombre());
+        alumnoDb.setApellido(alumno.getApellido());
+        alumnoDb.setEmail(alumno.getEmail());
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
+    }
+
+}
